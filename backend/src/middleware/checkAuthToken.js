3ce6203151aea3e1f2d334c378579fd645302e10
Sathya LK <rajavslk@gmail.com>
2020-06-01 14:53:30 +0530

    bug fix(checkAuthTokenMiddleware): admin query

--- backend/src/middleware/checkAuthToken.js
+++ backend/src/middleware/checkAuthToken.js
@@ -1,21 +1,21 @@
-const { verify } = require('jsonwebtoken');
-const adminModel = require('../models/admin');
+const { verify } = require("jsonwebtoken");
+const adminModel = require("../models/admin");
 
 module.exports = async (req, res, next) => {
   try {
     // const token = req.headers['Authorization'].split(' ')[1];
-    const token = req.headers.authorization.split(' ')[1];
+    const token = req.headers.authorization.split(" ")[1];
 
     const { user } = await verify(token, process.env.JWT_SECRET);
-    console.log(user);
-    const userExist = await adminModel.findOne({ email: user.email });
+
+    const userExist = await adminModel.findOne({ _id: user._id });
     if (!userExist) {
-      throw new Error('invalid Token');
+      throw new Error("invalid Token");
     }
-    console.log('user Exist', userExist);
+    console.log("user Exist", userExist);
     const tokenExist = userExist.tokens.includes(token);
     if (!tokenExist) {
-      throw new Error('invalid token');
+      throw new Error("invalid token");
     }
 
     req.user = userExist;
@@ -23,7 +23,7 @@ module.exports = async (req, res, next) => {
     next();
   } catch (err) {
     res.status(401).json({
-      status: 'fail',
+      status: "fail",
       message: err,
     });
   }
